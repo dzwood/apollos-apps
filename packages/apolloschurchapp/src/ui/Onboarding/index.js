2ef4eec085fcacdd50fd0c6b60ba7cc53a5e0ce5
Conrad VanLandingham <hi@convan.me>
2019-04-16 23:49:52 -0500

    use SafeAreaView on onboarding

--- packages/apolloschurchapp/src/ui/Onboarding/index.js
+++ packages/apolloschurchapp/src/ui/Onboarding/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import Swiper from 'react-native-swiper';
+import { StyleSheet } from 'react-native';
 import {
   withTheme,
   BackgroundView,
   GradientOverlayImage,
 } from '@apollosproject/ui-kit';
+import { SafeAreaView } from 'react-navigation';
 
 import {
   AskNotificationsConnected,
@@ -21,16 +23,27 @@ const ThemedSwiper = withTheme(({ theme }) => ({
   activeDotColor: theme.colors.action.primary,
 }))(({ swiperRef, ...props }) => <Swiper ref={swiperRef} {...props} />);
 
+const forceInset = {
+  top: 'always',
+  bottom: 'always',
+};
+
 class Onboarding extends Component {
   static navigationOptions = () => ({
     title: 'Onboarding',
     header: null,
   });
 
-  constructor() {
-    super();
+  swiper = null;
 
-    this.swiper = null;
+  state = {
+    swiperVisible: false,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ swiperVisible: true });
+    }, 0);
   }
 
   // Creates ref to Swiper to be passed as a prop to children.
@@ -44,39 +57,44 @@ class Onboarding extends Component {
   handleOnPressPrimary = () => this.swiper.scrollBy(1);
 
   render() {
+    // nasty hack. Relevant github issues: https://github.com/leecade/react-native-swiper/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+content+invisible
+    if (!this.state.swiperVisible) return null;
+
     return (
       <BackgroundView>
-        <ThemedSwiper
-          loop={false}
-          /* Disables swipe gestures. We currently we dont display a back button so this is our
-           * only back navigation option. */
-          // scrollEnabled={false}
-          showsButtons={false}
-          swiperRef={this.setSwiperRef}
-        >
-          <AskNameConnected onPressPrimary={this.handleOnPressPrimary} />
-          <Features
-            imgSrc={{ uri: 'https://picsum.photos/1200/1200?random' }}
-            onPressPrimary={this.handleOnPressPrimary}
-          />
-          <AboutYouConnected
-            imgSrc={{ uri: 'https://picsum.photos/1200/1200?random' }}
-            onPressPrimary={this.handleOnPressPrimary}
-          />
-          <LocationFinderConnected onPressPrimary={this.handleOnPressPrimary}>
-            <GradientOverlayImage
-              source={'https://picsum.photos/640/640/?random'}
-            />
-          </LocationFinderConnected>
-          <AskNotificationsConnected
-            onPressPrimary={() => this.props.navigation.navigate('Home')}
-            primaryNavText={'Finish'}
+        <SafeAreaView style={StyleSheet.absoluteFill} forceInset={forceInset}>
+          <ThemedSwiper
+            loop={false}
+            /* Disables swipe gestures. We currently we dont display a back button so this is our
+             * only back navigation option. */
+            // scrollEnabled={false}
+            showsButtons={false}
+            swiperRef={this.setSwiperRef}
           >
-            <GradientOverlayImage
-              source={'https://picsum.photos/640/640/?random'}
+            <AskNameConnected onPressPrimary={this.handleOnPressPrimary} />
+            <Features
+              imgSrc={{ uri: 'https://picsum.photos/1200/1200?random' }}
+              onPressPrimary={this.handleOnPressPrimary}
             />
-          </AskNotificationsConnected>
-        </ThemedSwiper>
+            <AboutYouConnected
+              imgSrc={{ uri: 'https://picsum.photos/1200/1200?random' }}
+              onPressPrimary={this.handleOnPressPrimary}
+            />
+            <LocationFinderConnected onPressPrimary={this.handleOnPressPrimary}>
+              <GradientOverlayImage
+                source={'https://picsum.photos/640/640/?random'}
+              />
+            </LocationFinderConnected>
+            <AskNotificationsConnected
+              onPressPrimary={() => this.props.navigation.navigate('Home')}
+              primaryNavText={'Finish'}
+            >
+              <GradientOverlayImage
+                source={'https://picsum.photos/640/640/?random'}
+              />
+            </AskNotificationsConnected>
+          </ThemedSwiper>
+        </SafeAreaView>
       </BackgroundView>
     );
   }
