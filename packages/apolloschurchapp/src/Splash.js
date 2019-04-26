e0b50ee4183b41ffff3e902ec268d80346b60be2
Burke Shartsis <burke@differential.com>
2019-04-26 15:03:51 -0400

    update Splash screen to use new props.

--- packages/apolloschurchapp/src/Splash.js
+++ packages/apolloschurchapp/src/Splash.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { styled, GradientOverlayImage } from '@apollosproject/ui-kit';
+
 import UiSplash from './ui/Splash';
 
+const FullScreenImage = styled({
+  resizeMode: 'cover',
+  position: 'absolute',
+})(GradientOverlayImage);
+
 const Splash = ({ navigation }) => (
-  <UiSplash onPressPrimary={() => navigation.push('Auth')} />
+  <UiSplash
+    onPressPrimary={() => navigation.push('Auth')}
+    textColor={'white'}
+    BackgroundComponent={
+      <FullScreenImage source={'https://picsum.photos/375/812/?random'} />
+    }
+    primaryNavText={"Let's go!"}
+  />
 );
 
 Splash.navigationOptions = {
