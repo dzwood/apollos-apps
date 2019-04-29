43f4c23c4280814467d96f8d747ffe7662190703
Burke Shartsis <burke@differential.com>
2019-04-29 17:06:17 -0400

    update AskName to use SlideContent component

--- packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskName.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskName.js
@@ -2,38 +2,11 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 
-import {
-  styled,
-  withTheme,
-  Icon,
-  H2,
-  H5,
-  PaddedView,
-  TextInput,
-} from '@apollosproject/ui-kit';
+import { PaddedView, TextInput } from '@apollosproject/ui-kit';
 
-import Slide from '../../Slide';
-
-const BrandIcon = withTheme(({ theme }) => ({
-  name: 'brand-icon',
-  size: theme.sizing.baseUnit * 3.0,
-  fill: theme.colors.primary,
-  style: {
-    marginTop: theme.sizing.baseUnit * 2,
-    marginBottom: theme.sizing.baseUnit * 0.5,
-  },
-}))(Icon);
-
-const Title = styled(({ theme }) => ({
-  color: theme.colors.primary,
-}))(H2);
-
-const StyledH5 = styled(({ theme }) => ({
-  color: theme.colors.text.secondary,
-}))(H5);
+import Slide, { SlideContent } from '../../Slide';
 
 // memo = sfc PureComponent 💥
-// eslint-disable-next-line react/display-name
 const AskName = memo(
   ({
     onPressPrimary,
@@ -51,10 +24,12 @@ const AskName = memo(
 
     return (
       <Slide onPressPrimary={onPressPrimary} {...props}>
-        <PaddedView vertical={false}>
-          <BrandIcon />
-          <Title>{slideTitle}</Title>
-          <StyledH5>{description}</StyledH5>
+        <SlideContent
+          title={slideTitle}
+          description={description}
+          vertical={false}
+          icon
+        >
           <PaddedView horizontal={false}>
             <TextInput
               label={'First Name'}
@@ -87,7 +62,7 @@ const AskName = memo(
               }}
             />
           </PaddedView>
-        </PaddedView>
+        </SlideContent>
       </Slide>
     );
   }
@@ -112,5 +87,7 @@ AskName.defaultProps = {
   slideTitle: 'Welcome!',
   description: "Every relationship starts with a name. What's yours?",
 };
+
+AskName.displayName = 'AskName';
 
 export default AskName;
