01f5a5dccaf5ec817c42932fd0e9f4b649c16736
Burke Shartsis <burke@differential.com>
2019-04-29 16:30:26 -0400

    fix Features test naming

--- packages/apolloschurchapp/src/ui/Onboarding/slides/Features/Features.tests.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/Features/Features.tests.js
@@ -6,7 +6,7 @@ import Providers from 'apolloschurchapp/src/Providers';
 
 import Features from './Features';
 
-describe('The Onboarding SlideWrapper component', () => {
+describe('The Onboarding Features component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
