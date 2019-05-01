4b36a3fb34cb06e52a34c34052c24c099ad90872
Burke Shartsis <burke@differential.com>
2019-05-01 11:15:41 -0400

    Improved UX for AskNameConnected to show correct Next or Skip button plus tests 🎉
    Co-Authored-By: Vincent Wilson <vince@classyh.at>

--- packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskNameConnected.js
+++ packages/apolloschurchapp/src/ui/Onboarding/slides/AskName/AskNameConnected.js
@@ -10,71 +10,91 @@ import AskName from './AskName';
 import updateUserName from './updateUserName';
 
 // eslint-disable-next-line react/display-name
-const AskNameConnected = memo(({ onPressPrimary, ...props }) => (
-  <Query query={getUserProfile}>
-    {({ data: { currentUser = { profile: {} } } = {} }) => {
-      const { firstName, lastName } = currentUser.profile;
+const AskNameConnected = memo(
+  ({ onPressPrimary, onPressSecondary, ...props }) => (
+    <Query query={getUserProfile}>
+      {({ data: { currentUser = { profile: {} } } = {} }) => {
+        const { firstName, lastName } = currentUser.profile;
 
-      return (
-        <Mutation mutation={updateUserName}>
-          {(updateName) => (
-            <Formik
-              initialValues={{ firstName, lastName }}
-              validationSchema={Yup.object().shape({
-                firstName: Yup.string().required(
-                  'Your first name is required!'
-                ),
-                lastName: Yup.string().required('Your last name is required!'),
-              })}
-              enableReinitialize
-              onSubmit={async (variables, { setSubmitting, setFieldError }) => {
-                try {
-                  await updateName({ variables });
-                  onPressPrimary();
-                } catch (e) {
-                  const { graphQLErrors } = e;
-                  if (
-                    graphQLErrors.length &&
-                    graphQLErrors.find(({ message }) =>
-                      message.includes('User already exists')
-                    )
-                  ) {
-                    setFieldError(
-                      'firstName',
-                      'There was a problem sending your request'
-                    );
-                  } else {
-                    setFieldError(
-                      'firstName',
-                      'Unknown error. Please try again later.'
-                    );
+        return (
+          <Mutation mutation={updateUserName}>
+            {(updateName) => (
+              <Formik
+                initialValues={{ firstName, lastName }}
+                isInitialValid={() => !!(firstName && lastName)} // isInitialValid defaults to `false` this correctly checks for user data
+                validationSchema={Yup.object().shape({
+                  firstName: Yup.string().required(
+                    'Your first name is required!'
+                  ),
+                  lastName: Yup.string().required(
+                    'Your last name is required!'
+                  ),
+                })}
+                enableReinitialize
+                onSubmit={async (
+                  variables,
+                  { setSubmitting, setFieldError }
+                ) => {
+                  try {
+                    await updateName({ variables });
+                    onPressPrimary();
+                  } catch (e) {
+                    const { graphQLErrors } = e;
+                    if (
+                      graphQLErrors.length &&
+                      graphQLErrors.find(({ message }) =>
+                        message.includes('User already exists')
+                      )
+                    ) {
+                      setFieldError(
+                        'firstName',
+                        'There was a problem sending your request'
+                      );
+                    } else {
+                      setFieldError(
+                        'firstName',
+                        'Unknown error. Please try again later.'
+                      );
+                    }
                   }
-                }
-                setSubmitting(false);
-              }}
-            >
-              {({ submitForm, values, touched, errors, setFieldValue }) => (
-                <AskName
-                  onPressPrimary={() => submitForm()}
-                  firstName={firstName}
-                  lastName={lastName}
-                  values={values}
-                  touched={touched}
-                  errors={errors}
-                  setFieldValue={setFieldValue}
-                  {...props}
-                />
-              )}
-            </Formik>
-          )}
-        </Mutation>
-      );
-    }}
-  </Query>
-));
+                  setSubmitting(false);
+                }}
+              >
+                {({
+                  isValid,
+                  submitForm,
+                  values,
+                  touched,
+                  errors,
+                  setFieldValue,
+                }) => (
+                  <AskName
+                    onPressPrimary={isValid ? submitForm : null} // if form `isValid` show the primary nav button (next)
+                    onPressSecondary={
+                      // if form `!isValid` show the secondary nav button (skip)
+                      isValid ? null : onPressSecondary || onPressPrimary // if onPressSecondary exists use it else default onPressPrimary
+                    }
+                    firstName={firstName}
+                    lastName={lastName}
+                    values={values}
+                    touched={touched}
+                    errors={errors}
+                    setFieldValue={setFieldValue}
+                    {...props}
+                  />
+                )}
+              </Formik>
+            )}
+          </Mutation>
+        );
+      }}
+    </Query>
+  )
+);
 
 AskNameConnected.propTypes = {
   onPressPrimary: PropTypes.func,
+  onPressSecondary: PropTypes.func,
 };
 
 export default AskNameConnected;
