ae7a855325a2e16d843fde654b20d6ab5f651559
Conrad VanLandingham <hi@convan.me>
2019-05-01 23:28:49 -0500

    backtrack overriding default route

--- packages/apolloschurchapp/src/index.js
+++ packages/apolloschurchapp/src/index.js
@@ -47,7 +47,7 @@ const AppNavigator = createStackNavigator(
     LandingScreen,
   },
   {
-    initialRouteName: 'Onboarding',
+    initialRouteName: 'ProtectedRoute',
     mode: 'modal',
     headerMode: 'screen',
   }
