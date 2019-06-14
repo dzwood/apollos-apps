141e3f23b978f71f620e297582541bc917478374
Burke Shartsis <burke@differential.com>
2019-06-14 11:06:06 -0400

    clean up PhoneEntryConnected a bit

--- packages/apollos-ui-auth/src/SMS/PhoneEntryConnected.js
+++ packages/apollos-ui-auth/src/SMS/PhoneEntryConnected.js
@@ -1,28 +1,15 @@
 /* eslint-disable react/no-unused-prop-types */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import {
-  styled,
-  H6,
-  PaddedView,
-  BackgroundView,
-  TextInput,
-  ButtonLink,
-} from '@apollosproject/ui-kit';
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 
-import {
-  NextButtonRow,
-  NextButton,
-  TitleText,
-  PromptText,
-  BrandIcon,
-} from '../styles';
+import { BackgroundView } from '@apollosproject/ui-kit';
+
+import PhoneEntry from './PhoneEntry';
 
 const requestPin = gql`
   mutation requestPin($phone: String!) {
@@ -32,38 +19,9 @@ const requestPin = gql`
   }
 `;
 
-const FlexedSafeAreaView = styled({
-  flex: 1,
-})(SafeAreaView);
-const forceInset = { top: 'always' };
-
-const LegalText = styled({
-  width: '70%',
-})(H6);
-
 class PhoneEntryConnected extends Component {
   static propTypes = {
-    brand: PropTypes.node,
-    authTitleText: PropTypes.string,
-    smsPromptText: PropTypes.string,
-    smsPolicyInfo: PropTypes.node,
-    allowPassword: PropTypes.bool,
-    smsPasswordLoginPrompt: PropTypes.node,
     screenProps: PropTypes.shape({}), // we'll funnel screenProps into props
-  };
-
-  static defaultProps = {
-    brand: <BrandIcon />,
-    authTitleText: 'Have we met before?',
-    smsPromptText:
-      "Let's get you signed in using your mobile number. We'll text you a code to make login super easy!",
-    smsPolicyInfo: (
-      <LegalText>
-        {"We'll never share your information or contact you (unless you ask!)."}
-      </LegalText>
-    ),
-    allowPassword: true,
-    smsPasswordLoginPrompt: "I'd rather use my email and a password",
   };
 
   validationSchema = Yup.object().shape({
@@ -73,9 +31,9 @@ class PhoneEntryConnected extends Component {
     ),
   });
 
-  get flatProps() {
-    return { ...this.props, ...(this.props.screenProps || {}) };
-  }
+  // get flatProps() {
+  //   return { ...this.props, ...(this.props.screenProps || {}) };
+  // }
 
   handleOnSubmit = (mutate) => async (
     { phone },
@@ -99,17 +57,20 @@ class PhoneEntryConnected extends Component {
   };
 
   render() {
-    const {
-      brand,
-      authTitleText,
-      smsPromptText,
-      smsPolicyInfo,
-      allowPassword,
-      smsPasswordLoginPrompt,
-    } = this.flatProps;
+    // const {
+    //   brand,
+    //   authTitleText,
+    //   smsPromptText,
+    //   smsPolicyInfo,
+    //   allowPassword,
+    //   smsPasswordLoginPrompt,
+    // } = this.flatProps;
 
     return (
-      <KeyboardAvoidingView style={StyleSheet.absoluteFill} behavior="padding">
+      <KeyboardAvoidingView
+        style={StyleSheet.absoluteFill}
+        behavior={'padding'}
+      >
         <BackgroundView>
           <Mutation mutation={requestPin}>
             {(mutate) => (
@@ -122,48 +83,18 @@ class PhoneEntryConnected extends Component {
                   setFieldValue,
                   handleSubmit,
                   values,
-                  isSubmitting,
-                  isValid,
+                  // isSubmitting,
+                  // isValid,
                   touched,
                   errors,
                 }) => (
-                  <FlexedSafeAreaView forceInset={forceInset}>
-                    <ScrollView>
-                      <PaddedView>
-                        {brand}
-                        <TitleText>{authTitleText}</TitleText>
-                        <PromptText padded>{smsPromptText}</PromptText>
-
-                        <TextInput
-                          autoFocus
-                          autoComplete="tel"
-                          label="Mobile Number"
-                          type="phone"
-                          returnKeyType="next"
-                          onSubmitEditing={this.handleAdvance}
-                          enzblesReturnKeyAutomatically
-                          error={touched.phone && errors.phone}
-                          onChangeText={(text) => setFieldValue('phone', text)}
-                          value={values.phone}
-                        />
-                      </PaddedView>
-                      {allowPassword ? (
-                        <PaddedView>
-                          <ButtonLink onPress={this.handlePasswordLoginPress}>
-                            {smsPasswordLoginPrompt}
-                          </ButtonLink>
-                        </PaddedView>
-                      ) : null}
-                    </ScrollView>
-                    <NextButtonRow>
-                      {smsPolicyInfo}
-                      <NextButton
-                        onPress={handleSubmit}
-                        disabled={isSubmitting || !isValid}
-                        loading={isSubmitting}
-                      />
-                    </NextButtonRow>
-                  </FlexedSafeAreaView>
+                  <PhoneEntry
+                    values={values}
+                    setFieldValue={setFieldValue}
+                    touched={touched}
+                    errors={errors}
+                    onPressNext={handleSubmit}
+                  />
                 )}
               </Formik>
             )}
