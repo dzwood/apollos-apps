fa0529c162162f02ebd65ff7ad1c7c386f34fa28
Burke Shartsis <burke@differential.com>
2019-06-24 11:07:50 -0400

    add default story and test over Verification component

---
+++ packages/apollos-ui-auth/src/SMS/Verification.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@apollosproject/ui-storybook';
+
+import Verification from './Verification';
+
+storiesOf('ui-auth/SMS/Verification', module).add('default', () => (
+  <Verification setFieldValue={() => {}} />
+));
+// .add('confirmationTitleText', () => (
+//   <Verification
+//     setFieldValue={() => {}}
+//     confirmationTitleText={'A Custom Title'}
+//   />
+// ))
+// .add('confirmationPromptText', () => (
+//   <Verification
+//     setFieldValue={() => {}}
+//     confirmationPromptText={'Boom custom prompty text boom'}
+//   />
+// ))
+// .add('disabled', () => (
+//   <Verification setFieldValue={() => {}} onPressNext={() => {}} disabled />
+// ))
+// .add('errors', () => (
+//   <Verification
+//     setFieldValue={() => {}}
+//     errors={{ code: 'Boom Error Boom' }}
+//   />
+// ))
+// .add('isLoading', () => (
+//   <Verification setFieldValue={() => {}} onPressNext={() => {}} isLoading />
+// ))
+// .add('onPressNext', () => (
+//   <Verification setFieldValue={() => {}} onPressNext={() => {}} />
+// ))
+// .add('values', () => (
+//   <Verification
+//     setFieldValue={() => {}}
+//     values={{ code: 'Boom value.code boom' }}
+//   />
+// ));
---
+++ packages/apollos-ui-auth/src/SMS/Verification.tests.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Providers } from '../testUtils';
+
+import Verification from './Verification';
+
+describe('The Auth Verification component', () => {
+  it('should render', () => {
+    const tree = renderer.create(
+      <Providers>
+        <Verification setFieldValue={jest.fn()} />
+      </Providers>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  // it('should render with custom confirmationTitleText', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         confirmationTitleText={'Custom Title'}
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render with a custom confirmationPromptText', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         confirmationPromptText={'Boom custom prompty text boom'}
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render as disabled', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         onPressNext={jest.fn()}
+  //         disabled
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render in an error state', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         errors={{ phone: 'Boom Error Boom' }}
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render in a loading state', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         onPressNext={jest.fn()}
+  //         isLoading
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render in a next button', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification setFieldValue={jest.fn()} onPressNext={jest.fn()} />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render a custom smsPolicyInfo component', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         smsPolicyInfo={
+  //           <H6 style={{ color: 'salmon' }}>Boom custom legalese boom</H6> // eslint-disable-line react-native/no-inline-styles, react-native/no-color-literals
+  //         }
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+  // it('should render with a value', () => {
+  //   const tree = renderer.create(
+  //     <Providers>
+  //       <Verification
+  //         setFieldValue={jest.fn()}
+  //         values={{ code: 'Boom value.code boom' }}
+  //       />
+  //     </Providers>
+  //   );
+  //   expect(tree).toMatchSnapshot();
+  // });
+});
