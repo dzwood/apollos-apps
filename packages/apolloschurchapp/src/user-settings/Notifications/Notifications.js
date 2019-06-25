f73c2616205edc6b0eca38d78fee84ea0b1dbefb
Michael Neeley <micneeley14@gmail.com>
2019-06-25 10:54:54 -0400

    added some files

---
+++ packages/apolloschurchapp/src/user-settings/Notifications/Notifications.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+import { Text } from 'react-native';
+import { Switch } from '@apollosproject/ui-kit';
+
+const Notifications = memo(() => <Text>Notifications</Text>);
+
+Notifications.displayName = 'Notifications';
+
+export default Notifications;
---
+++ packages/apolloschurchapp/src/user-settings/Notifications/Notifications.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@apollosproject/ui-storybook';
+
+import Notifications from './Notifications';
+
+storiesOf('user-settings/Notifications', module).add('default', () => (
+  <Notifications />
+));
