91a4de4aee96613bd91f367552191192d4779a64
Michael Neeley <micneeley14@gmail.com>
2019-06-25 13:57:28 -0400

    added a couple of placeholder comments

--- packages/apolloschurchapp/src/user-settings/Notifications/index.js
+++ packages/apolloschurchapp/src/user-settings/Notifications/index.js
@@ -6,6 +6,8 @@ import { GET_NOTIFICATIONS_ENABLED } from '@apollosproject/ui-notifications';
 import Notifications from './Notifications';
 
 const NotificationsConnected = memo(() => (
+  // TODO: add toggle mutation
+  // TODO: this is the wrong query, we'll use a new one that pulls from Rock
   <Query query={GET_NOTIFICATIONS_ENABLED}>
     {({ data: { notificationsEnabled = false } = {} }) => (
       <Notifications notificationsEnabled={notificationsEnabled} />
