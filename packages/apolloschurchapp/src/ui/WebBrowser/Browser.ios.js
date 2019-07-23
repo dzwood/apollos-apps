6bc3b323986ec314a00a6baaf279814253aff54f
Michael Neeley <micneeley14@gmail.com>
2019-07-23 11:29:27 -0400

    Removes Buggy Safari Package

--- packages/apolloschurchapp/src/ui/WebBrowser/Browser.ios.js
+++ packages/apolloschurchapp/src/ui/WebBrowser/Browser.ios.js
@@ -2,17 +2,9 @@ import { Linking } from 'react-native';
 import SafariView from 'react-native-safari-view';
 
 const Browser = {
-  openURL: async (url, options) => {
-    try {
-      await SafariView.isAvailable();
-      SafariView.show({
-        url,
-        ...options,
-      });
-    } catch (e) {
-      if (!Linking.canOpenURL(url)) throw new Error('URL not supported');
-      Linking.openURL(url);
-    }
+  openURL: async (url) => {
+    if (!Linking.canOpenURL(url)) throw new Error('URL not supported');
+    Linking.openURL(url);
   },
 };
 
