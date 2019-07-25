1a8795ccdc7f165f6b411e3502ade161d26fc0f3
Conrad VanLandingham <hi@convan.me>
2019-07-25 10:10:19 -0500

    Add multiple Stretchy support and support for headers and footers

--- packages/apollos-ui-kit/src/StretchyView/StretchyView.stories.js
+++ packages/apollos-ui-kit/src/StretchyView/StretchyView.stories.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { storiesOf } from '@apollosproject/ui-storybook';
-import { ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
 import {
   ConnectedImage,
   GradientOverlayImage,
@@ -17,18 +17,17 @@ const imageStyle = { width: '100%', aspectRatio: 1 };
 
 storiesOf('StretchyView', module)
   .add('stretchy as a background', () => (
-    <StretchyView
-      StretchyComponent={
-        <GradientOverlayImage
-          overlayColor="white"
-          overlayType="featured"
-          source={{ uri: 'https://picsum.photos/600/600' }}
-          style={imageStyle}
-        />
-      }
-    >
-      {(scrollViewProps) => (
+    <StretchyView>
+      {({ Stretchy, ...scrollViewProps }) => (
         <ScrollView {...scrollViewProps}>
+          <Stretchy background>
+            <GradientOverlayImage
+              overlayColor="white"
+              overlayType="featured"
+              source={{ uri: 'https://picsum.photos/id/233/600/600' }}
+              style={imageStyle}
+            />
+          </Stretchy>
           <PaddedView>
             <H1 padded>Well hello there!</H1>
             <H2 padded>
@@ -43,24 +42,25 @@ storiesOf('StretchyView', module)
     </StretchyView>
   ))
   .add('stretchy as a header', () => (
-    <StretchyView
-      StretchyComponent={
-        <ConnectedImage
-          style={imageStyle}
-          source={{ uri: 'https://picsum.photos/600/600' }}
-        />
-      }
-    >
-      {({ stretchyHeight, ...scrollViewProps }) => (
+    <StretchyView>
+      {({ Stretchy, ...scrollViewProps }) => (
         <ScrollView {...scrollViewProps}>
-          <View
-            style={{
-              height: stretchyHeight,
-              alingItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <H2>Text on top of the stretchy!</H2>
+          <View>
+            <Stretchy>
+              <ConnectedImage
+                style={imageStyle}
+                source={{ uri: 'https://picsum.photos/id/342/600/600' }}
+              />
+            </Stretchy>
+            <View
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <H2>Text on top of the stretchy!</H2>
+            </View>
           </View>
           <PaddedView>
             <H1 padded>Well hello there!</H1>
@@ -71,6 +71,58 @@ storiesOf('StretchyView', module)
             <H3 padded>Hello there!</H3>
             <H1 padded>Try to scroll me around.</H1>
           </PaddedView>
+        </ScrollView>
+      )}
+    </StretchyView>
+  ))
+  .add('stretchy as a footer', () => (
+    <StretchyView>
+      {({ Stretchy, ...scrollViewProps }) => (
+        <ScrollView {...scrollViewProps}>
+          <PaddedView>
+            <H1 padded>Well hello there!</H1>
+            <H2 padded>
+              Hello there!
+              {'\n'}
+            </H2>
+            <H3 padded>Hello there!</H3>
+            <H1 padded>Try to scroll me around.</H1>
+          </PaddedView>
+          <Stretchy stretchOn="bottom">
+            <ConnectedImage
+              style={imageStyle}
+              source={{ uri: 'https://picsum.photos/id/342/600/600' }}
+            />
+          </Stretchy>
+        </ScrollView>
+      )}
+    </StretchyView>
+  ))
+  .add('multiple stretchies', () => (
+    <StretchyView>
+      {({ Stretchy, ...scrollViewProps }) => (
+        <ScrollView {...scrollViewProps}>
+          <Stretchy stretchyKey="header" stretchOn="top">
+            <ConnectedImage
+              style={imageStyle}
+              source={{ uri: 'https://picsum.photos/id/342/600/600' }}
+            />
+          </Stretchy>
+          <PaddedView>
+            <H1 padded>Well hello there!</H1>
+            <H2 padded>
+              Hello there!
+              {'\n'}
+            </H2>
+            <H3 padded>Hello there!</H3>
+            <H1 padded>Try to scroll me around.</H1>
+          </PaddedView>
+          <Stretchy stretchyKey="footer" stretchOn="bottom">
+            <ConnectedImage
+              style={imageStyle}
+              source={{ uri: 'https://picsum.photos/id/342/600/600' }}
+            />
+          </Stretchy>
         </ScrollView>
       )}
     </StretchyView>
