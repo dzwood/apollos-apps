383223c0e7e8af94d4bb45ecf93ce97b5c3ee236
Burke Shartsis <burke@differential.com>
2019-09-03 17:05:22 -0400

    add search field to discover page

--- packages/apolloschurchapp/src/tabs/discover/Discover.js
+++ packages/apolloschurchapp/src/tabs/discover/Discover.js
@@ -1,11 +1,47 @@
 import React, { PureComponent } from 'react';
+import { Platform, StyleSheet, View } from 'react-native';
 import { Query } from 'react-apollo';
 import { get } from 'lodash';
 
-import { FeedView, BackgroundView } from '@apollosproject/ui-kit';
+import {
+  FeedView,
+  BackgroundView,
+  styled,
+  PaddedView,
+  SearchInput,
+} from '@apollosproject/ui-kit';
 
 import TileContentFeed from './TileContentFeed';
 import GET_CONTENT_CHANNELS from './getContentChannels';
+
+const HeaderBorder = styled(({ theme }) => ({
+  paddingBottom: 8,
+  /* It's unclear why this is necessary but without it the layout breaks on both platforms. Limited
+   * research suggest that without a background color the shadows don't know what to blend with so
+   * the view collapses. */
+  backgroundColor: theme.colors.background.paper,
+  // Renders the same shadows that React Navigation does.
+  ...Platform.select({
+    ios: {
+      shadowColor: 'rgba(0, 0, 0, 0.3)',
+      shadowOpacity: 0.85,
+      shadowRadius: 0,
+      shadowOffset: {
+        width: 0,
+        height: StyleSheet.hairlineWidth,
+      },
+    },
+    android: {
+      elevation: 4,
+    },
+  }),
+}))(PaddedView);
+
+// This element is used to clip the Android shadow in every directection except the bottom.
+const ClipAndroidElevationFix = styled({
+  paddingBottom: StyleSheet.hairlineWidth,
+  overflow: 'hidden',
+})(View);
 
 const childContentItemLoadingState = {
   title: '',
@@ -16,6 +52,13 @@ const feedItemLoadingState = {
   name: '',
   isLoading: true,
 };
+
+const styles = StyleSheet.create({
+  header: {
+    borderBottomWidth: 0,
+    elevation: 0,
+  },
+});
 
 class Discover extends PureComponent {
   renderItem = ({ item }) => (
@@ -33,6 +76,11 @@ class Discover extends PureComponent {
   render() {
     return (
       <BackgroundView>
+        <ClipAndroidElevationFix>
+          <HeaderBorder vertical={false}>
+            <SearchInput />
+          </HeaderBorder>
+        </ClipAndroidElevationFix>
         <Query query={GET_CONTENT_CHANNELS} fetchPolicy="cache-and-network">
           {({
             error,
@@ -58,6 +106,7 @@ class Discover extends PureComponent {
 
 Discover.navigationOptions = {
   title: 'Discover',
+  headerStyle: styles.header,
 };
 
 export default Discover;
