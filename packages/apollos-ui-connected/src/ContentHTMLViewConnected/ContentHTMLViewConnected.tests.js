6f3f5b4c23d74b28cabf5e9706d1bbb7551bdf7c
Jordan Wade <jjwade31@gmail.com>
2020-02-05 16:17:55 -0600

    Add ContentHTMLViewConnected tests

---
+++ packages/apollos-ui-connected/src/ContentHTMLViewConnected/ContentHTMLViewConnected.tests.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import wait from 'waait';
+
+import { Providers } from '../utils/testUtils';
+import GET_CONTENT_ITEM_CONTENT from './getContentItemContent';
+
+import ContentHTMLViewConnected from './ContentHTMLViewConnected';
+
+describe('ContentHTMLViewConnected component', () => {
+  it('renders ContentHTMLViewConnected', async () => {
+    const mock = {
+      request: {
+        query: GET_CONTENT_ITEM_CONTENT,
+        variables: { contentId: '1' },
+      },
+      result: {
+        data: {
+          node: {
+            __typename: 'DevotionalContentItem',
+            id: '1',
+            htmlContent: '<b>Some content!</b>',
+          },
+        },
+      },
+    };
+    const tree = renderer.create(
+      <Providers mocks={[mock]}>
+        <ContentHTMLViewConnected contentId={'1'} />
+      </Providers>
+    );
+    await wait(0); // wait for response from graphql
+    expect(tree).toMatchSnapshot();
+  });
+});
