9d0c8aa60dab8f3f8589cd734b09ad9e82f2673f
Jordan Wade <jjwade31@gmail.com>
2020-02-24 10:31:35 -0600

    Fix LikeButtonConnected test warnings

--- packages/apollos-ui-connected/src/LikeButtonConnected/LikeButtonConnected.tests.js
+++ packages/apollos-ui-connected/src/LikeButtonConnected/LikeButtonConnected.tests.js
@@ -40,6 +40,7 @@ describe('LikeButtonConnected component', () => {
             fragmentMatcher,
           })
         }
+        resolvers={{}}
       >
         <LikeButtonConnected itemId={'1'} />
       </Providers>
@@ -71,6 +72,7 @@ describe('LikeButtonConnected component', () => {
             fragmentMatcher,
           })
         }
+        resolvers={{}}
       >
         <LikeButtonConnected itemId={'1'} />
       </Providers>
